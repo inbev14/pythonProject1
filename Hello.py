f082ac96e040c5e4a9a22c99e4ba5fbcbee86c5e
inbev14 <inbev@i.ua>
2022-09-17 20:06:07 +0300

    test

--- Hello.py
+++ Hello.py
@@ -1,10 +1,10 @@
 import sys
 print("Hello world!")
-a = 100
+a: int = 100
 a = b = [1, 2, 3]
 print(id(a))
 print(id(b))
 print(sys.getrefcount(a))
 print(sys.getrefcount(b))
 for i in a:
-    
+    pass
