de603a1b949df2e7d5b16a217c164ba7bd29cbcc
inbev14 <inbev@i.ua>
2022-09-30 16:57:21 +0300

    hmwrk6 working, but with punctuation

--- Vadym_Rukavytsia_hmwrk6.py
+++ Vadym_Rukavytsia_hmwrk6.py
@@ -5,7 +5,7 @@ filename = 'text_example.txt'
 
 
 def find_longest_words(name_file):
-    
+    """Find the longest words and return length of them also list with the longest words"""
     file_path = path.join(getcwd(), 'other_files', name_file)
     with open(file_path) as file:
         highest_len = 0
@@ -17,7 +17,7 @@ def find_longest_words(name_file):
         for word in file_text:
             if len(word) == highest_len:
                 highest_len_words.append(word)
-        return (f'You opened file {file_path}. \nThe longest words in the file "{name_file}" has {highest_len} symbols.'
-                f'The longest words are {highest_len_words}.')
+        return (f'You opened file {file_path}. \nThe longest words in "{name_file}" has {highest_len} symbols.'
+                f' The longest words are: {highest_len_words}.')
     
 print(find_longest_words(filename))
