8db96932bf06e2dd334ff11dae3d9e672a30efdd
inbev14 <inbev@i.ua>
2022-10-07 19:29:14 +0300

    all working

--- Vadym_Rukavytsia_hmwrk8.py
+++ Vadym_Rukavytsia_hmwrk8.py
@@ -1,11 +1,39 @@
 import re
-filename = 'mbox.txt'
-with open(filename) as f:
-    text = f.read()
-    pattern = r'[\w+]?[\.]?[\w+]?[\.]?\w+@\w+[\.]?[\w+]?[\.]?[\w+]?[\.]?[\w+]?'
-    emails = re.findall(pattern, text)
-    count_of_at = re.findall(r'@', text)
-    # check = r'@.\w+'
-    print(f'Found {len(emails)}, must be {len(count_of_at)}')
-    # new_text = re.sub(pattern, "", text)
-    print(emails)
+from prettytable import PrettyTable
+
+
+def read_file(file: str):
+    """read file and return list with emails"""
+    with open(file) as f:
+        text = f.read()
+        pattern = r'[\w*\.*]*@[\w*\.]*'
+        return re.findall(pattern, text)
+    
+    
+def make_dictionary_with_emails(full_emails: list):
+    """enter emails and return normalized dictionary with emails"""
+    dict_emails = {}
+    for email in full_emails:
+        key = email.split('@')[1]
+        dict_emails.setdefault(key, [])
+        if not email in dict_emails[key]:
+            dict_emails[key].append(email)
+    return dict_emails
+    
+    
+def main(dict_emails):
+    email_table = PrettyTable()
+    email_table.field_names = ['Domen', 'number of emails']
+    for domen, email in dict_emails.items():
+        email_table.add_row([domen, len(email)])
+    return email_table
+
+
+if __name__ == '__main__':
+    filename = 'mbox.txt'
+    list_emails = read_file(filename)
+    dict_emails = make_dictionary_with_emails(list_emails)
+    print(main(dict_emails))
+    
+    
+    
