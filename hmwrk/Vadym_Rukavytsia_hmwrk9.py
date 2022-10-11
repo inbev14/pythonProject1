8b5227eb1d88cdf8bb00f106e6250111edd8444d
inbev14 <inbev@i.ua>
2022-10-11 20:59:23 +0300

    working, alright

--- hmwrk/Vadym_Rukavytsia_hmwrk9.py
+++ hmwrk/Vadym_Rukavytsia_hmwrk9.py
@@ -1,45 +1,42 @@
 """ Imitation of two square cubes and make statistic of 1000 iterations"""
-
+# import namedtuple
 import collections
 import random
 from prettytable import PrettyTable
 
 
-def dice():
+def dice() -> int:
     """imitation of two square cubes"""
     a = random.randint(1, 6)
     b = random.randint(1, 6)
     return a + b
 
 
-def make_statistic(throws=1000):
+def make_statistic(throws: int) -> tuple[list[tuple[int, int]], int]:
     """throw dice and remember in statistic"""
     statistic_throw = collections.Counter()
-    throw = 0
-    while throw < throws:
+    for _ in range(throws):
         statistic_throw[dice()] += 1
-        throw += 1
     statistic_throw_sorted = sorted(statistic_throw.items())
     return statistic_throw_sorted, throws
 
 
-def make_table(data: list, throws: int):
+def make_table(data: list, throws: int) -> PrettyTable:
     """return table with data"""
     table = PrettyTable()
     table.field_names = ['number', 'percent simulations']
-    # print(len(data))
     for i in data:
         table.add_row([i[0], round(float(i[1] / throws * 100), 3)])
     table.add_column('expected data', [2.78, 5.56, 8.3, 11.11, 13.89, 16.67, 13.89, 11.11, 8.3, 5.56, 2.78])
     return table
 
 
-def main():
+def main() -> PrettyTable:
     """main controller"""
     stats, throws = make_statistic(number_of_throws)
     return make_table(stats, throws)
 
 
 if __name__ == '__main__':
-    number_of_throws = 1000
+    number_of_throws = int(input("Enter qty throws: "))
     print(main())
---
+++ test_files/dfgd.py
@@ -0,0 +1,3 @@
+from hmwrk import Vadym_Rukavytsia_hmwrk9
+
+Vadym_Rukavytsia_hmwrk9.main()
